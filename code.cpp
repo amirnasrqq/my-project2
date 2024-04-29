799b93513f521923ffb6c371081e91f12e357c4c
amirhosseinnasr <a12mirnasr1234@gmail.com>
2024-04-29 21:59:39 +0330

    test commit

--- code.cpp
+++ code.cpp
@@ -2,10 +2,18 @@
 using namespace std ;
 //hellodwsfsfwfsevfwese 
 //this is a proje . member of grop amirhossein nasr and ali shahabi 
+class done1
+{
+public ;
+
+done1() ;
+};
+
 int main () {
 
 cout << "hello" ;
 
+done1 ob() ;
 return 0 ;
 
 }
