d0c68675d68670bf0a418f6bdab484151b09eb84
ali <shhabyly73@gmail.com>
2024-05-07 14:57:21 +0330

    add teacher
    added teacher

--- code.cpp
+++ code.cpp
@@ -4,51 +4,89 @@ using namespace std;
 
 void questiontesti();
 void descripQues();
+void list();
 
-int main() {
+int main() 
+{
     // for inter ostad /pasword
+    string userType;
     string username;
     string password;
     bool loggedIn = false;
 
-    while (!loggedIn) {
+    while (!loggedIn) 
+	{
+		cout << "Enter user type ( student / teacher ) : ";
+		cin >> userType;
         cout << "Enter your username: ";
         cin >> username;
         cout << "Enter your password: ";
         cin >> password;
 
-        if (username == "A" && password == "1234") {
+        if (username == "A" && password == "1234" && userType == "teacher" ) 
+		{
             loggedIn = true;
-            cout << "Logged in successfully! continue..." << endl;
-        } else {
-            cout << "Invalid username or password. Please try again." << endl;
+            cout << "hello teacher , welcome!" << endl;
+    		
+			int choose = 0;
+			cout << "for tst enter *1*\nfor dscrip enter *2*\nfor finish *-1*\n";
+			while (choose != -1)
+			{
+				cin >> choose;
+				switch (choose )
+				{
+					case 1 :
+					{
+					    questiontesti(); 	
+						break;
+					}
+					case 2 :
+					{
+   				 		descripQues();
+						break;
+					}
+				}
+			}
+        //edameeeeeeeeeee..........
+        
+        
         }
+		else if(username == "A" && password == "1234" && userType == "student")
+		{
+			loggedIn = true;
+            cout << "hallo student , welcome to the exam!" << endl;
+            //.............?????
+                
+        }
+        else
+        {
+        	cout << "Invalid username or password. Please try again." << endl;
+		}
     }
-    cout << "\nHello, welcome to the exam\n";
-cout <<"\n-------------------------------------\n" ;
-    questiontesti(); 
-    descripQues();
-    
+
     return 0;
 }
 
-void questiontesti() {
+void questiontesti()
+{
     int numberOfStudents, numberOfQuestions;
     cout << "Enter the number of students forquestiontesti : ";
     cin >> numberOfStudents;
-    cout << "Enter the number of questionsquestiontesti: ";
+    cout << "Enter the number of questiontesti: ";
     cin >> numberOfQuestions;
 
     string* questions = new string[numberOfQuestions];
     char* answerKey = new char[numberOfQuestions];
     char** studentAnswers = new char*[numberOfStudents];
 
-    for (int i = 0; i < numberOfStudents; ++i) {
+    for (int i = 0; i < numberOfStudents; ++i)
+	{
         studentAnswers[i] = new char[numberOfQuestions];
     }
 
     // Enter the questions and correct answers
-    for (int i = 0; i < numberOfQuestions; ++i) {
+    for (int i = 0; i < numberOfQuestions; ++i)
+	{
         cout << " Boss :Enter question  " << (i + 1) << ": ";
         cin.ignore(); // Ignores the newline character from the previous input
         getline(cin, questions[i]);
@@ -57,29 +95,32 @@ void questiontesti() {
     }
 
     // Enter student answers and calculate scores
-    for (int s = 0; s < numberOfStudents; ++s) {
+    for (int s = 0; s < numberOfStudents; ++s)
+	{
         int score = 0;
         cout << "Enter answers for student " << (s + 1) << ": \n";
-        for (int q = 0; q < numberOfQuestions; ++q) {
+        for (int q = 0; q < numberOfQuestions; ++q)
+		{
             cin >> studentAnswers[s][q];
-            if (studentAnswers[s][q] == answerKey[q]) {
+            if (studentAnswers[s][q] == answerKey[q])
+			{
                 score++;
             }
         }
         cout << "Score for student " << (s + 1) << ": " << score << " out of " << numberOfQuestions << endl;
     }
 
-   
     delete[] questions;
     delete[] answerKey;
-    for (int i = 0; i < numberOfStudents; ++i) {
+    for (int i = 0; i < numberOfStudents; ++i)
+	{
         delete[] studentAnswers[i];
     }
     delete[] studentAnswers;
 }
 
-
-void descripQues() {
+void descripQues()
+{
     int numberOfStudents, numberOfDescripQues;
     cout << "Enter the number of students: ";
     cin >> numberOfStudents;
@@ -89,11 +130,13 @@ void descripQues() {
     string* questions = new string[numberOfDescripQues];
     string* correctAnswers = new string[numberOfDescripQues];
     string** studentAnswers = new string*[numberOfStudents];
-    for (int i = 0; i < numberOfStudents; ++i) {
+    for (int i = 0; i < numberOfStudents; ++i)
+	{
         studentAnswers[i] = new string[numberOfDescripQues];
     }
 
-    for (int i = 0; i < numberOfDescripQues; ++i) {
+    for (int i = 0; i < numberOfDescripQues; ++i)
+	{
         cout << "Enter question " << (i+1) << ": ";
         cin.ignore(); 
         getline(cin, questions[i]);
@@ -101,33 +144,43 @@ void descripQues() {
         getline(cin, correctAnswers[i]);
     }
 
-    for (int s = 0; s < numberOfStudents; ++s) {
+    for (int s = 0; s < numberOfStudents; ++s)
+	{
         cout << "Answer from student " << (s+1) << ": \n";
-        for (int q = 0; q < numberOfDescripQues; ++q) {
+        for (int q = 0; q < numberOfDescripQues; ++q)
+		{
             cout << questions[q] << endl;
             getline(cin, studentAnswers[s][q]);
-
-
-for (int s = 0; s < numberOfStudents; ++s) {
-    int score = 0;
-    cout << "Answer from student " << (s+1) << ": \n";
-    for (int q = 0; q < numberOfDescripQues; ++q) {
-        cout << questions[q] << endl;
-        getline(cin, studentAnswers[s][q]);
-        if (studentAnswers[s][q] == correctAnswers[q]) {
-            score++; 
-        }
-    }
-    cout << "Score for student " << (s+1) << ": " << score << " out of " << numberOfDescripQues << endl;
-}
+            
+			for (int s = 0; s < numberOfStudents; ++s)
+			{
+    			int score = 0;
+    			cout << "Answer from student " << (s+1) << ": \n";
+    			for (int q = 0; q < numberOfDescripQues; ++q)
+				{
+    			    cout << questions[q] << endl;
+    			    getline(cin, studentAnswers[s][q]);
+    			    if (studentAnswers[s][q] == correctAnswers[q])
+					{
+        			    score++; 
+        			}
+    			}
+    			cout << "Score for student " << (s+1) << ": " << score << " out of " << numberOfDescripQues << endl;
+			}
 
         }
     }
 
     delete[] questions;
     delete[] correctAnswers;
-    for (int i = 0; i < numberOfStudents; ++i) {
+    for (int i = 0; i < numberOfStudents; ++i)
+	{
         delete[] studentAnswers[i];
     }
     delete[] studentAnswers;
+}
+
+void list()
+{
+	cout << "\n1_ask the question .\n2_  (edameeeeee)  "
 }
